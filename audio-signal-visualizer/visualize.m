8ce1e18f8c84ae15b2c986799c1db02e4a140df0
Kiran Pisupati <kiranp.ece@gmail.com>
2025-08-25 23:17:18 +0530

    visualize.m

---
+++ audio-signal-visualizer/visualize.m
@@ -0,0 +1,10 @@
+
+
+```matlab
+% AUDIO SIGNAL VISUALIZER (MATLAB)
+% This script plots waveform and spectrogram of an audio file.
+
+clear; clc;
+
+audioFile = fullfile('audio','example_waveform.png'); % placeholder
+disp('This script would read an audio file and plot waveform + spectrogram');
